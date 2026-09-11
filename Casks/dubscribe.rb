9046022e6bd4f552ea1a6f49259fb904c6184ade
Bonecrusher <charlie@localhost>
2026-09-12 07:00:38 +1000

    dubscribe 0.7.1
    A fix for the Pause media setting launching Apple Music.

--- Casks/dubscribe.rb
+++ Casks/dubscribe.rb
@@ -1,6 +1,6 @@
 cask "dubscribe" do
-  version "0.7.0"
-  sha256 "4aff576a4ffa2a74263f2b3f0eff9e1d727b415cc401771ed355f401bd53f3c2"
+  version "0.7.1"
+  sha256 "acb2dd7b46055ad9caf9a3901ad052a473bd4aee62a1ee02e155d7f274cd4002"
 
   url "https://github.com/dangercharlie/DubScribe/releases/download/v#{version}/DubScribe-macOS-arm64.dmg"
   name "DubScribe"
